74cb7c9f4516a9ecb7ca0864f586ffcb4595a8ad
vasilyKurdyaev <kurdyaev.exceedteam@gmail.com>
2020-05-26 15:06:43 +0300

    Updates

---
+++ precache-manifest.2f7a152c51fd5f77cfe5b381b75eb69f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5bf6d71964821c46ad9ce629d08e9bc",
+    "url": "/react-todolist/index.html"
+  },
+  {
+    "revision": "f61b6b2913210fdcd30b",
+    "url": "/react-todolist/static/css/main.0be7b7f4.chunk.css"
+  },
+  {
+    "revision": "447dba838b4b5c7f1553",
+    "url": "/react-todolist/static/js/2.ad331804.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todolist/static/js/2.ad331804.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f61b6b2913210fdcd30b",
+    "url": "/react-todolist/static/js/main.c1a2fec0.chunk.js"
+  },
+  {
+    "revision": "81501db65ff6640f42a5",
+    "url": "/react-todolist/static/js/runtime-main.3d0c7696.js"
+  }
+]);
